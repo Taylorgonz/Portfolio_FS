5d9f268da608769f2adb6582646092492fa51ae2
taylorgonz <hello@taylorgonz.com>
2021-08-13 15:24:23 -0400

    axios image display working

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,17 @@
-import './App.css';
-import Hero from "./Components/Hero/index"
-import About from "./Components/About/index"
-import Photography from "./Components/Photography/index"
+import './App.css';;
+import Hero from "./Components/Hero/index";
+import About from "./Components/About/index";
+import Photography from "./Components/Photography/index";
+import Upload from './Components/Upload/index'
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
 
 function App() {
   return (
     <div className="App">
+      <Switch>
+        <Route exact path="/upload" render={()=> <Upload/>}/>
+      </Switch>
 
       <section className="child">
         <Hero />
--- client/src/Components/Upload/index.js
+++ client/src/Components/Upload/index.js
@@ -4,10 +4,10 @@ import { Container, Row, Col } from "react-bootstrap";
 import API from '../../utils/API'
 import axios from 'axios'
 
-import Hero from '../Hero/index'
 
 
-const Photography = () => {
+
+const Upload = () => {
     const [photos, setPhotos] = useState([]);
     const getPics = () => {
         axios.get('/api/photos')
@@ -28,20 +28,11 @@ console.log(photos);
 
 
 return (
-    <Container id="photography" className="photographyContainer" >
-        <h1 className='photographyTitle'>Photography</h1>
-        <Row className='photoRow'>
-        {photos.map((photo, i) =>
-            <Col>
-
-                <img className="photoImages" key={photo.id} src={photo.url}></img>
-
-            </Col>
-        )}
-        </Row>
+    <Container >
+    
 
     </Container>
 )
 }
 
-export default Photography;
+export default Upload;
