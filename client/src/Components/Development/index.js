bf8ffa69312909c984a631d91ef7555d1abc398b
taylorgonz <hello@taylorgonz.com>
2021-09-01 15:00:05 -0400

    lose the favicon

---
+++ client/src/Components/Development/index.js
@@ -0,0 +1,18 @@
+import React, { useState, useEffect } from "react"
+import "./style.css"
+import { Container, Row, Col, Carousel } from "react-bootstrap";
+import API from '../../utils/API'
+import axios from 'axios'
+
+
+
+const WebDev = ({ setModalImage }) => {
+
+
+
+ render(
+
+ )
+}
+
+export default WebDev;
